9697bacd9e85a11b325b78ecfb6341334793e530
ianbautista <ianfbautista@gmail.com>
2020-08-13 12:03:57 -0700

    fixed syntax error on CartContext.Provider value. MVP Done

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
 
 	return (
 		<ProductContext.Provider value={{ products, addItem }}>
-			<CartContext.Provider value={cart}>
+			<CartContext.Provider value={{ cart }}>
 				<div className="App">
 					<Navigation />
 
